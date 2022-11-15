4c38f3d027576bfc28228979584fcdf7b9a94c09
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-11-15 15:02:36 -0600

    power rule function

--- powerRule.py
+++ powerRule.py
@@ -1,3 +1,17 @@
 from splitTerms import splitTerms
+# given a list of separated terms in form ax^n, differentiate. 
 
-print(splitTerms("5x+2+3"))
+def powerRule(function):
+    x=0
+    for i in function:
+        indexOfCarrot = i.find("^")
+        if indexOfCarrot>-1:
+            exponent = int(i[indexOfCarrot+1:len(i)])
+        elif i.__contains__("x"): exponent = 1
+        else: exponent = 0
+        if exponent == 0: function[x] = ""
+        if exponent == 1: function[x] = i[:len(i)-1]
+        x+=1
+    print(function)
+
+powerRule(splitTerms("5x^2+5x+5"))
