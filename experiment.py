87d4e6cc481369d87f7c34e5b649e19a22039865
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-11-15 00:33:47 -0600

    experimentation

--- experiment.py
+++ experiment.py
@@ -1,7 +1,19 @@
-def fun(x):
-    return x**2+5*x+5
+def splitTerms():
+    myFun = input("please input your formula. Refer to readme for proper styling:\n")
 
-def main():
-    print(fun(1))
+    splitLocations = []
 
-main()
+    myFunSplit = []
+
+    for i in range(0,len(myFun)):
+        if myFun[i] == "+":
+            splitLocations.append(i)
+
+
+    myFunSplit.append(myFun[0:splitLocations[0]])
+    for i in range(0,len(splitLocations)-1):
+        myFunSplit.append(myFun[splitLocations[i]+1:splitLocations[i+1]])
+    myFunSplit.append(myFun[splitLocations[len(splitLocations)-1]+1:])
+
+    print(myFunSplit)
+splitTerms()
