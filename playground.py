29ed0783c93dc08f02e6d6d4125787e793ab980e
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-11-21 11:16:34 -0600

    tweaks

--- playground.py
+++ playground.py
@@ -1,16 +1,12 @@
 from trigRule import trigRule
 from powerRule import powerRule
 
-class myfunction():
-    def __init__(self,f):
-        function = f
-
 def typecheck(term):
-    if term[0].find("sin") >-1 or term.find("cos") >-1 or term.find("tan")>-1:
+    if term.find("sin") >-1 or term.find("cos") >-1 or term.find("tan")>-1:
         return "trig"
     return "nontrig"
 
 
-myfun = "60x^2-50x"
+myfun = "-sinx"
 if typecheck(myfun) == "trig": print(trigRule(myfun))
 else: print(powerRule(myfun))
